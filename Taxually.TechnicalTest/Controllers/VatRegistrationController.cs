8e54061a5135f840b0995849f314770c98485a35
Németh Balázs <nemethbalazs.nb@gmail.com>
2025-02-17 10:34:47 +0100

    Logging in controller

--- Taxually.TechnicalTest/Controllers/VatRegistrationController.cs
+++ Taxually.TechnicalTest/Controllers/VatRegistrationController.cs
@@ -9,7 +9,7 @@ namespace Taxually.TechnicalTest.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class VatRegistrationController(CountryFactory countryFactory) : ControllerBase
+    public class VatRegistrationController(CountryFactory countryFactory, ILogger<VatRegistrationController> logger) : ControllerBase
     {
         /// <summary>
         /// Registers a company for a VAT number in a given country
@@ -19,6 +19,7 @@ namespace Taxually.TechnicalTest.Controllers
         {
             try
             {
+                logger.LogInformation($"Request handled: country: \"{request.Country}\", company name: \"{request.CompanyName}\", company id: \"{request.CompanyId}\"");
                 await countryFactory.HandleAsync(request);
             }
             catch (CountryNotSupportedException ex)
